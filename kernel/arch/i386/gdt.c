9f67cf9028855ed467114b11bf3782c14c1e9986
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-09-09 15:55:10 +0200

    load tss

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -2,6 +2,7 @@
 #include <kernel/types.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <kernel/libc/string.h>
 
 #define GDT_ACCESS_TYPE_SYSTEM 0
 #define GDT_ACCESS_TYPE_CODEDATA 1
@@ -12,6 +13,7 @@
 #define GDT_FLAG_SIZE_16BIT 0
 #define GDT_FLAG_SIZE_32BIT 1
 
+#define GDT_ACCESS_TYPE_TSS32_AVAIL 0x09
 #define GDT_ACCESS_PRESENT(x) (0x0080 * (x))
 #define GDT_ACCESS_TYPE(x) (0x0010 * (x))
 #define GDT_ACCESS_EXEC(x) (0x0008 * (x))
@@ -44,11 +46,49 @@
 	 GDT_ACCESS_ACCESSED(1) | GDT_FLAG_GRAN(1) |                         \
 	 GDT_FLAG_SIZE(GDT_FLAG_SIZE_32BIT) | GDT_FLAG_LONG(0))
 
+#define GDT_TSS                                                            \
+	(GDT_ACCESS_PRESENT(1) | GDT_ACCESS_TYPE(GDT_ACCESS_TYPE_SYSTEM) | \
+	 GDT_ACCESS_EXEC(1) | GDT_ACCESS_RING(0) |                         \
+	 GDT_ACCESS_TYPE_TSS32_AVAIL | GDT_FLAG_GRAN(0) | 0 /* db */ |     \
+	 GDT_FLAG_LONG(0))
+
 struct gdtr {
 	u16 limit;
 	u32 base;
 } __attribute__((packed));
 
+struct tss {
+	u32 prev_tss;
+	u32 esp0;
+	u32 ss0;
+	u32 esp1;
+	u32 ss1;
+	u32 esp2;
+	u32 ss2;
+	u32 cr3;
+	u32 eip;
+	u32 eflags;
+	u32 eax;
+	u32 ecx;
+	u32 edx;
+	u32 ebx;
+	u32 esp;
+	u32 ebp;
+	u32 esi;
+	u32 edi;
+	u32 es;
+	u32 cs;
+	u32 ss;
+	u32 ds;
+	u32 fs;
+	u32 gs;
+	u32 ldt;
+	u16 trap;
+	u16 iomap_base;
+
+} __attribute__((packed));
+
+static struct tss _tss;
 static u64 _gdt[16];
 
 static u64 gdt_encode(u32 base, u32 limit, u16 flags)
@@ -85,15 +125,25 @@ static void load_gdt(u64 *gdt, size_t entries)
 	lgdt((u32)(uintptr_t)gdt, (entries * sizeof(*gdt)) - 1);
 }
 
+static void init_tss(void)
+{
+	memset(&_tss, 0, sizeof(_tss));
+
+	_tss.ss0 = I386_KERNEL_DATA_SELECTOR;
+	_tss.esp0 = (u32) (uintptr_t) &_stack_top;
+}
+
 void init_segments(void)
 {
+	init_tss();
+
 	_gdt[0] = gdt_encode(0, 0, 0);
 	_gdt[I386_GDT_KERNEL_CODE_IDX] = gdt_encode(0, 0x000fffff, GDT_CODE(0));
 	_gdt[I386_GDT_KERNEL_DATA_IDX] = gdt_encode(0, 0x000fffff, GDT_DATA(0));
 	_gdt[I386_GDT_USER_CODE_IDX] = gdt_encode(0, 0x000fffff, GDT_CODE(3));
 	_gdt[I386_GDT_USER_DATA_IDX] = gdt_encode(0, 0x000fffff, GDT_DATA(3));
-	// TODO TSS segment
+	_gdt[I386_GDT_TSS_IDX] = gdt_encode((uintptr_t) &_tss, sizeof(_tss) - 1, GDT_TSS);
 
-	load_gdt(_gdt, 5);
+	load_gdt(_gdt, 6);
 	_reload_segments(I386_KERNEL_CODE_SELECTOR, I386_KERNEL_DATA_SELECTOR);
 }
