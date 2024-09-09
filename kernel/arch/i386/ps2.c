2b4a27ae0148f03f75264d6bcc52b25e91816fd6
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-09-09 13:05:45 +0200

    cleanup i386 ps2 code

--- kernel/arch/i386/ps2.c
+++ kernel/arch/i386/ps2.c
@@ -4,39 +4,29 @@
 #define PS2_DATA_PORT 0x60
 #define PS2_STATUS_PORT 0x64
 
-static void sendb(u16 port, u8 b)
-{
-	outb(port, b);
-}
-
-static u8 recvb(u16 port)
-{
-	return inb(port);
-}
-
 void ps2_send(u8 b)
 {
-	sendb(PS2_DATA_PORT, b);
+	outb(PS2_DATA_PORT, b);
 }
 
 void ps2_send_cmd(u8 b)
 {
-	sendb(PS2_STATUS_PORT, b);
+	outb(PS2_STATUS_PORT, b);
 }
 
 u8 ps2_recv(void)
 {
-	return recvb(PS2_DATA_PORT);
+	return inb(PS2_DATA_PORT);
 }
 
 bool ps2_canrecv(void)
 {
-	u8 b = recvb(PS2_STATUS_PORT);
+	u8 b = inb(PS2_STATUS_PORT);
 	return b & 0b00000001;
 }
 
 bool ps2_cansend(void)
 {
-	u8 b = recvb(PS2_STATUS_PORT);
+	u8 b = inb(PS2_STATUS_PORT);
 	return !(b & 0b00000010);
 }
