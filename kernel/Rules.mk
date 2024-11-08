27fc8d3656e65a5a4d3ea3e47a5561593fd1338e
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-11-08 14:55:51 +0100

    move sync file to its own dir

--- kernel/Rules.mk
+++ kernel/Rules.mk
@@ -4,8 +4,7 @@ d		:= $(dir)
 
 OBJS_$(d)	:= \
 	$(d)/main.o $(d)/keycode.o $(d)/printk.o $(d)/ps2.o $(d)/tty.o $(d)/acpi.o $(d)/panic.o $(d)/sched.o \
-	$(d)/spinlock.o $(d)/irq.o $(d)/debug.o $(d)/loop.o $(d)/serial.o $(d)/vma.o $(d)/elf.o $(d)/signal.o \
-	$(d)/condvar.o $(d)/list.o $(d)/mutex.o $(d)/monitor.o
+	$(d)/irq.o $(d)/debug.o $(d)/loop.o $(d)/serial.o $(d)/vma.o $(d)/elf.o $(d)/signal.o $(d)/list.o
 DEPS_$(d)	:= $(OBJS_$(d):%.o=%.d)
 CLEAN		:= $(CLEAN) $(OBJS_$(d)) $(DEPS_$(d))
 
@@ -28,6 +27,9 @@ dir		:= $(d)/syscall
 include		$(dir)/Rules.mk
 
 dir		:= $(d)/user
+include		$(dir)/Rules.mk
+
+dir		:= $(d)/sync
 include		$(dir)/Rules.mk
 
 -include $(DEPS_$(d))
